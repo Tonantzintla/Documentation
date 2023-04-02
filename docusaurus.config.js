040b23fc2c8e4499b4245232280d6037b9eb173c
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-04-02 21:38:23 +0200

    Workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "sirius", // Usually your GitHub org/user name.
-  projectName: "sirius-docs", // Usually your repo name.
+  organizationName: "SiriusDevelopmentGroup", // Usually your GitHub org/user name.
+  projectName: "Sirius-Docs", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
