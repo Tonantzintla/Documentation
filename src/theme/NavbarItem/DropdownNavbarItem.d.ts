582987d931da22fcdc39df08b6e9a0f5a81270c6
Gigi <47110839+DarthGigi@users.noreply.github.com>
2023-06-24 23:24:17 +0200

    Change styling

---
+++ src/theme/NavbarItem/DropdownNavbarItem.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import type { Props } from "@theme/NavbarItem/DropdownNavbarItem";
+export default function DropdownNavbarItem({ mobile, ...props }: Props): JSX.Element;
